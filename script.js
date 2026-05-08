05b4c00675ef3be6b92d91a18a45d4c491a3b076
marketinginsaigon <65582031+marketinginsaigon@users.noreply.github.com>
2026-05-08 16:13:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -309,15 +309,6 @@ async function submitOrder(event) {
     showSubmitStatus("error", "Vui lòng điền đủ họ tên, số điện thoại và địa chỉ giao hàng.");
     return;
   }
-
-  if (GOOGLE_SHEET_WEB_APP_URL.includes("https://script.google.com/macros/s/AKfycbyBHZZypb8bNGnbC8UenQUWYY5F0xHTJ6kknlcEP9AeGRwBAj_nZhySq_AjA1s6I6R7tQ/exec")) {
-    showSubmitStatus(
-        "error",
-        "Chưa cấu hình URL Google Apps Script..."
-    );
-    return;
-}  
-
   const orderCode = `DH-${Date.now()}`;
 
   const orderPayload = {
